4505da32f4dcda21858c0bf868641d29e7f4c6ec
Sylvester Agbo <105360514+slybullion@users.noreply.github.com>
2022-06-23 02:54:33 +0100

    JAVASCRIPT INTRO

---
+++ javascriptintro.js
@@ -0,0 +1,7 @@
+const clientname = prompt('what is your name:');
+const height = prompt('what is your height:');
+const country = prompt('what country are you from:');
+
+alert(`${clientname}
+${height}
+${country}`)
